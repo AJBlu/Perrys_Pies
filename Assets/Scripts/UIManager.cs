1740e10af18d14ffac614e2ec4e9ba6dd2a5c5e5
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-09-10 15:35:01 -0700

    UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject elevatorPanelHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevatorPanelHolder.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
