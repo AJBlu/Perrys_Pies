3f9657b355ff4107ce99d07e90d9739ed99ab616
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-09-17 14:49:30 -0700

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
         if (pressedButton == 1)
         {
             checkForDupes();
-            destroySingleObjects();
+            //destroySingleObjects();
         }
     }
 
