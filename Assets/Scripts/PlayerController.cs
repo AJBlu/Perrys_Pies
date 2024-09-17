d6e0b685e6b917b5fabbef124fe9b95161155447
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-09-17 16:25:06 -0700

    Barebones Unity Template, for Testing Level Design

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -69,12 +69,6 @@ public class PlayerController : MonoBehaviour
         isCrouched = false;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
