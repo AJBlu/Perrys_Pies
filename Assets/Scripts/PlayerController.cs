a1f0e6ce16f174206ffaa9441ab73e1217e8d564
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-09-19 15:14:10 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -296,46 +296,10 @@ public class PlayerController : MonoBehaviour
             target.transform.position = this.transform.position + (this.transform.forward * canThrowStrength);
             Debug.Log("Throwing Pie Scent");
         }
-        StartCoroutine(SimulateProjectile(throwable));
+        
     }
 
-    IEnumerator SimulateProjectile(GameObject Projectile)
-    {
-        // Short delay added before Projectile is thrown
-        //yield return new WaitForSeconds(1.5f);
-
-        Instantiate(Projectile, this.transform.position, Quaternion.identity);
-
-        // Move projectile to the position of throwing object + add some offset if needed.
-        Projectile.transform.position = this.transform.position + new Vector3(0, 0.0f, 0);
-
-        // Calculate distance to target
-        float target_Distance = Vector3.Distance(Projectile.transform.position, target.transform.position);
-
-        // Calculate the velocity needed to throw the object to the target at specified angle.
-        float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / 9.8f);
-
-        // Extract the X  Y componenent of the velocity
-        float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
-        float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
-
-        // Calculate flight time.
-        float flightDuration = target_Distance / Vx;
-
-        // Rotate projectile to face the target.
-        Projectile.transform.rotation = Quaternion.LookRotation(target.transform.position - Projectile.transform.position);
-
-        float elapse_time = 0;
-
-        while (elapse_time < flightDuration)
-        {
-            Projectile.transform.Translate(0, (Vy - (9.8f * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
-
-            elapse_time += Time.deltaTime;
-
-            yield return null;
-        }
-    }
+    
 
     private void FixedUpdate()
     {
