b55aad79dccdac59d42f59560e23c22fb1d3f943
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-09-19 18:37:46 -0700

    Dumpster Fire Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -295,11 +295,13 @@ public class PlayerController : MonoBehaviour
             target.transform.position = this.transform.position + (this.transform.forward * canThrowStrength);
             Debug.Log("Throwing Pie Scent");
         }
-        StartCoroutine(throwObject(throwable));
+        //StartCoroutine(throwObject(throwable));
     }
 
     public float lerpFloat = 0f;
 
+    //THIS CODE IS COMPLETE AND UTTER GARBAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /*
     public IEnumerator throwObject(GameObject distraction)
     {
         Instantiate(distraction, this.transform.position, Quaternion.identity);
@@ -308,21 +310,20 @@ public class PlayerController : MonoBehaviour
         Vector3 initTargetPos = target.transform.position;
 
         float target_Distance = Vector3.Distance(distraction.transform.position, initTargetPos);
-        
-        //float fraction = target_Distance
 
         Vector3 interpolatedPosition = Vector3.Lerp(initPlayerPos, initTargetPos, target_Distance);
 
         lerpFloat += .01f;
 
-        while (!Mathf.Approximately(target_Distance, 0f))
+        while (!Mathf.Approximately(target_Distance, lerpFloat))
         {
-
-
-            yield return null;
+            Debug.Log("Moving forward a bit.");
+            distraction.transform.position += (distraction.transform.forward);
+            lerpFloat += .01f;
         }
-        
+        yield return null;
     }
+    */
 
     private void FixedUpdate()
     {
