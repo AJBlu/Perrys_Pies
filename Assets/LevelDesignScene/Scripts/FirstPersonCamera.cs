0bfdbc7c4a8285da59a3113cb4cb4ed8754eddf7
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-10-08 12:17:57 -0700

    ugh

--- Assets/LevelDesignScene/Scripts/FirstPersonCamera.cs
+++ Assets/LevelDesignScene/Scripts/FirstPersonCamera.cs
@@ -8,23 +8,16 @@ public class FirstPersonCamera : MonoBehaviour
     public float mouseSensitivity = 2f;
     float cameraVerticalRotation = 0f;
 
-    public Quaternion ogRotation;
-
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
-        ogRotation = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!player.GetComponent<PlayerController>().uiManager.menuOpen &&
-            !player.GetComponent<PlayerController>().uiManager.givingHint)
-        {
             float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
             float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
@@ -32,13 +25,6 @@ public class FirstPersonCamera : MonoBehaviour
             cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
             transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
-            player.Rotate(Vector3.up * inputX);
-        }
-    }
-
-    public void resetRotation()
-    {
-        cameraVerticalRotation = 0;
-        player.SetPositionAndRotation(player.gameObject.GetComponent<PlayerController>().ogPos, ogRotation);
+        player.Rotate(Vector3.up * inputX);
     }
 }
