1898693acc7f29d8095aa6a036a7bbabfb2486c0
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-10-17 15:47:21 -0700

    Deterrent now delays once thrown, then checks if Perry is nearby.

--- Assets/ExampleDeterrent.cs
+++ Assets/ExampleDeterrent.cs
@@ -12,16 +12,35 @@ public class ExampleDeterrent : MonoBehaviour
     public float DeterrentRadius;
 
     public UnityEvent<float> DeterrentItemHit;
+
+
     private void Awake()
     {
-        StartCoroutine("StartDeterrent");
+        //remove this when deterrent is attached to actual deterrent
+        OnDeterrentThrown();
     }
 
-    private  IEnumerator StartDeterrent()
+    //to be triggered when deterrent is thrown 
+    public void OnDeterrentThrown()
+    {
+        StartCoroutine(activateDeterrent());
+    }
+
+    private IEnumerator activateDeterrent()
+    {
+        yield return new WaitForSeconds(DeterrentDelay);
+        Collider[] objectsInRadius = Physics.OverlapSphere(this.transform.position, DeterrentRadius);
+
+        foreach (Collider collider in objectsInRadius)
+        {
+            if (collider.tag == "Perry") StartDeterrent();
+        }
+    }
+
+    private  void StartDeterrent()
     {
         var Perry = GameObject.FindGameObjectWithTag("Perry");
         DeterrentItemHit.AddListener(Perry.GetComponent<PerryNav>().OnDeterrentItemHit);
-        yield return new WaitForSeconds(DeterrentDelay);
         DeterrentItemHit.Invoke(StopDuration);
         DeterrentItemHit.RemoveListener(Perry.GetComponent<PerryNav>().OnDeterrentItemHit);
 
