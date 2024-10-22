4c9f00c319905987834df97f7f12de4e010fda15
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-10-22 16:10:42 -0700

    m

---
+++ Assets/StateMachine/Deterrent_Events.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Deterrent_Events : MonoBehaviour
+{
+    public UnityEvent DeterrentTriggered;
+
+    public float DeterrentDuration;
+
+    public float DeterrentRadius;
+
+    public float DeterrentDelay;
+
+
+
+}
