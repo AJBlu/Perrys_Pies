b6d73898e9f3f1ff5f2af28c9dec9ce9d54ad2b5
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-11-12 16:47:05 -0800

    Made the basic Controls screen

--- Assets/LevelDesignScene/Scripts/UIManagerClone.cs
+++ Assets/LevelDesignScene/Scripts/UIManagerClone.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManagerClone : MonoBehaviour
 {
+    GameObject mainMenu;
+    GameObject controls;
+    GameObject controlsButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenu = GameObject.FindGameObjectWithTag("MainMenu");
+        controls = GameObject.Find("Controls");
+        controlsButton = controls.transform.Find("StartGame").gameObject;
+
+        controls.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        controlsButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
+        controlsButton.GetComponent<Button>().interactable = false;
     }
 
     // Update is called once per frame
@@ -21,6 +33,15 @@ public class UIManagerClone : MonoBehaviour
     {
         Debug.Log("You'll be back... and I'll be waiting.");
         Application.Quit();
+    }
+
+    public void showControls()
+    {
+        mainMenu.SetActive(false);
+        controls.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        controlsButton.GetComponent<Image>().color = new Color(1, 0, 0, 1);
+        controlsButton.GetComponent<Button>().interactable = true;
+
     }
 
     public void startGame()
