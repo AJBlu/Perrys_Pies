3c784da46c71ef98a070fb66951241a981eee781
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-12-03 21:52:09 -0800

    Update PlayerController.cs

--- Assets/LevelDesignScene/Scripts/PlayerController.cs
+++ Assets/LevelDesignScene/Scripts/PlayerController.cs
@@ -416,6 +416,7 @@ public class PlayerController : MonoBehaviour
                     hit.collider.GetComponent<Outline>().OutlineWidth = 5;
                 else if (hit.collider.tag == "Elevator" && hasPieTin) 
                     hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
+                else hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
 
                 pickupUI.SetActive(true);
             }
