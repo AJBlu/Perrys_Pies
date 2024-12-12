6fb1c505e74f6941c5570c70e1798fa86bd5175c
jchagerman <116826038+jchagerman@users.noreply.github.com>
2024-12-11 19:15:20 -0800

    Lined up Title Logo with Animation Logo

--- Assets/LevelDesignScene/Scripts/TitleManager.cs
+++ Assets/LevelDesignScene/Scripts/TitleManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class TitleManager : MonoBehaviour
@@ -8,7 +9,9 @@ public class TitleManager : MonoBehaviour
     public GameObject menuScreen;
     public List<VideoClip> videos;
 
-    
+    public GameObject startButton;
+    public GameObject quitButton;
+    public GameObject logo;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,32 @@ public class TitleManager : MonoBehaviour
         var videoPlayer = camera.GetComponent<VideoPlayer>();
 
         videoPlayer.clip = videos[0];
+
+        startButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        quitButton.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        logo.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
+        StartCoroutine(FadeMenuToFullAlpha(-26f));
     }
 
     void EndReached(VideoPlayer vp)
     {
         vp.clip = videos[1];
         vp.isLooping = true;
+    }
+
+    public IEnumerator FadeMenuToFullAlpha(float start)
+    {
+        while (startButton.GetComponent<Image>().color.a < 1.0f)
+        {
+            startButton.GetComponent<Image>().color = new Color
+                (1, 1, 1, startButton.GetComponent<Image>().color.a - (Time.deltaTime / start));
+            quitButton.GetComponent<Image>().color = new Color
+                (1, 1, 1, quitButton.GetComponent<Image>().color.a - (Time.deltaTime / start));
+            logo.GetComponent<Image>().color = new Color
+                (1, 1, 1, logo.GetComponent<Image>().color.a - (Time.deltaTime / start));
+            yield return null;
+        }
     }
 
     // Update is called once per frame
